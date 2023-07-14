beb28de3ad07404f1ac59f1142711b4f632bb43d
Thomas Rose-Meyer <prettiest.robot@gmail.com>
2023-07-13 23:35:50 -0400

    all logic working

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,7 +11,7 @@ INSERT INTO role (title, salary, department_id) VALUES
     ('Marketing Coordinator', 45000.00, 2);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-    ('John', 'Doe', 1, NULL),
+    ('John', 'Doe', 1, 3),
     ('Jane', 'Smith', 2, 1),
     ('Michael', 'Johnson', 3, 1),
     ('Emily', 'Williams', 4, 2),
